bd9cec3b3f6495a8fdf362f8f9cabfcaf369020d
winshine0326 <shane10340@gmail.com>
2024-07-29 03:07:58 +0900

    feat: practice1 2단계(함수로 분리하기) 완료

--- practice/practice1.js
+++ practice/practice1.js
@@ -1,18 +1,28 @@
-// Mission 1
-let a = prompt("첫번째 값 입력")
-let sign = prompt("계산 부호")
-let b = prompt("두번쨰 값 입력")
+let a
+let sign
+let b
 
-a = parseInt(a)
-b = parseInt(b)
+function input(){
+  a = prompt("첫번째 값 입력")
+  sign = prompt("계산 부호")
+  b = prompt("두번쨰 값 입력")
 
-if(sign == '+')
-  alert(a+sign+b+'='+(a+b))
-else if(sign == '-')
-  alert(a+sign+b+'='+(a-b))
-else if(sign == '*')
-  alert(a+sign+b+'='+(a*b))
-else if(sign == '/')
-  alert(a+sign+b+'='+(a/b))
-else if(sign == '%')
-  alert(a+sign+b+'='+(a%b))
+  a = parseInt(a)
+  b = parseInt(b)
+}
+
+function calc(a,b,sign){
+  if(sign == '+')
+    alert(a+sign+b+'='+(a+b))
+  else if(sign == '-')
+    alert(a+sign+b+'='+(a-b))
+  else if(sign == '*')
+    alert(a+sign+b+'='+(a*b))
+  else if(sign == '/')
+    alert(a+sign+b+'='+(a/b))
+  else if(sign == '%')
+    alert(a+sign+b+'='+(a%b))
+}
+
+input()
+calc(a,b,sign)
