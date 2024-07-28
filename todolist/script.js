0e7f09228bf72db8a4ff97a7dd4909e3b2ff50f8
winshine0326 <shane10340@gmail.com>
2024-07-29 07:12:35 +0900

    feat: 텍스트 없이 +버튼을 누를 경우 경고창 띄우기

--- todolist/script.js
+++ todolist/script.js
@@ -1,17 +1,20 @@
 function addTodolist() {
-    let li = document.createElement('li') 
-    let text = document.createElement('span') 
+    let li = document.createElement('li')
+    let text = document.createElement('span')
     let btn = document.createElement('button')
 
     li.appendChild(btn)
     li.appendChild(text)
 
     const todo = document.querySelector('#textcontainer')
-    text.innerHTML = todo.value //li안에 들어갈 span 태그 안에 텍스트박스 value 값 넣어주기
-
-    const todolist = document.querySelector('#todolist')
-    todolist.appendChild(li) // ul에 li 연결
-    todo.value = '' // 텍스트 박스 비우기
+    if (!todo.value)
+        alert("해야할 일을 작성해주세요!")
+    else {
+        text.innerHTML = todo.value //li안에 들어갈 span 태그 안에 텍스트박스 value 값 넣어주기
+        const todolist = document.querySelector('#todolist')
+        todolist.appendChild(li) // ul에 li 연결
+        todo.value = '' // 텍스트 박스 비우기
+    }
     console.log(todolist)
 }
 
