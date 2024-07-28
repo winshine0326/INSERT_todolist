2b57d458f9a56288974770faacbb5ab55f2f5a04
winshine0326 <shane10340@gmail.com>
2024-07-29 03:02:12 +0900

    feat: practice1 1단계(계산기) 완료

--- practice/practice1.js
+++ practice/practice1.js
@@ -1,1 +1,18 @@
 // Mission 1
+let a = prompt("첫번째 값 입력")
+let sign = prompt("계산 부호")
+let b = prompt("두번쨰 값 입력")
+
+a = parseInt(a)
+b = parseInt(b)
+
+if(sign == '+')
+  alert(a+sign+b+'='+(a+b))
+else if(sign == '-')
+  alert(a+sign+b+'='+(a-b))
+else if(sign == '*')
+  alert(a+sign+b+'='+(a*b))
+else if(sign == '/')
+  alert(a+sign+b+'='+(a/b))
+else if(sign == '%')
+  alert(a+sign+b+'='+(a%b))
