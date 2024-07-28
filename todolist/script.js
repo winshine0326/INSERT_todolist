16fd7581a7f10fa29ec79246966a062a20d1fac8
winshine0326 <shane10340@gmail.com>
2024-07-29 08:27:59 +0900

    feat: 삭제버튼 추가

--- todolist/script.js
+++ todolist/script.js
@@ -3,11 +3,15 @@ function addTodolist() {
     let li = document.createElement('li')
     let text = document.createElement('span')
     let btn = document.createElement('button')
+    let delbtn = document.createElement('button')
+
+    delbtn.className = 'delbtn'
+    delbtn.innerHTML = "삭제"
 
     li.appendChild(btn)
     li.appendChild(text)
-
-
+    li.appendChild(delbtn)
+    
     const todo = document.querySelector('#textcontainer')
     if (!todo.value)
         alert("해야할 일을 작성해주세요!")
@@ -24,21 +28,22 @@ function addTodolist() {
             li.style.textDecoration = "line-through"
             li.style.color = "#a1a1a1"
             btn.style.backgroundColor = "#e37e8e"
-            swt=false
+            swt = false
         } // 체크 안되어 있을 시 버튼 누르면 밑줄 긋기
-        else{
+        else {
             li.style.textDecoration = "none"
             li.style.color = "black"
             btn.style.backgroundColor = "white"
-            swt=true
+            swt = true
         } // 체크 되어 있을 시 버튼 누르면 밑줄 삭제
-
     }) // 버튼 한번 클릭 시 텍스트에 줄 긋기
 
-    
-
-    text.addEventListener('dblclick',()=>{
+    text.addEventListener('dblclick', () => {
         let newtext = prompt("변경할 내용을 입력해주세요")
-        text.innerHTML=newtext
-    })
+        text.innerHTML = newtext
+    }) // 텍스트 더블클릭 시 내용 수정
+
+    delbtn.addEventListener('click', () => {
+        todolist.removeChild(li)
+    }) // 딜리트 버튼 누를 시 리스트 삭제
 }
