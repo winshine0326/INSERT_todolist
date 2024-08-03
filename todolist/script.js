d361f97e1782bcebc2dc2abf5351daaa8c11568e
winshine0326 <shane10340@gmail.com>
2024-08-03 20:11:24 +0900

    refactor : 쿼리셀렉터 -> getElementById 로 변경

--- todolist/script.js
+++ todolist/script.js
@@ -12,12 +12,12 @@ function addTodolist() {
     li.appendChild(text)
     li.appendChild(delbtn)
     
-    const todo = document.querySelector('#textcontainer')
+    const todo = document.getElementById('textcontainer')
     if (!todo.value)
         alert("해야할 일을 작성해주세요!")
     else {
         text.innerHTML = todo.value //li안에 들어갈 span 태그 안에 텍스트박스 value 값 넣어주기
-        const todolist = document.querySelector('#todolist')
+        const todolist = document.getElementById('todolist')
         todolist.appendChild(li) // ul에 li 연결
         todo.value = '' // 텍스트 박스 비우기
     }
